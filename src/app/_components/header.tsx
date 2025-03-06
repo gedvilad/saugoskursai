60d468b3691727574235b3851c2d1aa4b3db74fa
gedvilad <gedvilad@gmail.com>
2025-03-06 14:19:03 +0200

    testing some things

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export function Header() {
+  const router = useRouter();
+  return (
+    <div className="m-4 flex border-b border-black text-lg">
+      <div className="m-2 border-2 border-black">LOGO</div>
+      <div className="flex w-full flex-wrap justify-end space-x-10 p-2">
+        <button
+          className="w-32 hover:text-xl"
+          onClick={() => router.push("/teorija")}
+        >
+          Button 1
+        </button>
+        <button className="w-32 rounded-md border-2 border-gray-200 hover:bg-gray-100">
+          Button 2
+        </button>
+        <button className="w-32 rounded-md border-2 border-gray-200 hover:bg-gray-100">
+          Button 3
+        </button>
+        <button className="w-32 rounded-md border-2 border-gray-200 hover:bg-gray-100">
+          Button 4
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Header;
