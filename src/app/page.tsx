27b1fcc5a56896a92a3e7257ebb8bd323d0a29d5
gedvilad <gedvilad@gmail.com>
2025-03-06 22:44:46 +0200

    fixed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
-
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
