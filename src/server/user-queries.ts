ecb996e41b7bdaaac1820873e2c2d3300ed2b324
gedvilad <gedvilad@gmail.com>
2025-03-07 10:08:06 +0200

    removed import

--- src/server/user-queries.ts
+++ src/server/user-queries.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import "server-only";
 import { db } from "~/server/db";
-import { users } from "./schema";
 export async function getUserByClerkId(id: string) {
   const user = await db.query.users.findFirst({
     where: (usersTable, { eq }) => eq(usersTable.clerk_id, id),
