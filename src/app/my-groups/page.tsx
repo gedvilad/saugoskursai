3f0140e1a1997920f920a5ee6f0e2426ba01db8a
gedvilad <gedvilad@gmail.com>
2025-03-10 20:40:19 +0200

    some ui changes with groups page

--- src/app/my-groups/page.tsx
+++ src/app/my-groups/page.tsx
@@ -125,7 +125,8 @@ export default function Home() {
     const data = (await res.json()) as ApiResponse;
 
     setGroups(data.groups);
-    setSelectedGroup(data.groups[0]!);
+    setSelectedGroup(data.groups[data.groups.length - 1]!);
+    setActiveTab("users");
     setIsCreating(false);
     setNewGroupName("");
   };
