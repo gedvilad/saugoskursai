05bd3cb15e5da30a2cd02a03e172bd92e32485e4
gedvilad <gedvilad@gmail.com>
2025-06-27 21:54:29 +0300

    maintenance mode

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,9 @@
+// pages/maintenance.tsx or app/maintenance/page.tsx (depending on your setup)
+export default function Maintenance() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>🚧 Site Under Maintenance</h1>
+      <p>We'll be back soon. Thanks for your patience!</p>
+    </div>
+  );
+}
